90d78a376131c66f6d5efe087d2111efe7a2e67b
M. Isa Gul <71321835+misagul@users.noreply.github.com>
2021-08-18 14:28:04 +0300

    Create hw8.sql

---
+++ sql/hw8.sql
@@ -0,0 +1,17 @@
+-- 1) CREATE TABLE employee (
+--  	id INTEGER,
+--  	name VARCHAR(50),
+--  	birthday DATE,
+--  	email VARCHAR(100)
+--    );
+
+-- 2) insert into employee (id, name, birthday, email) values (1, 'Dawna', '1978-05-18', 'dtwyning0@pen.io');
+--    ...
+--    insert into employee (id, name, birthday, email) values (50, 'Ileana', '1949-11-20', 'iziem1d@reverbnation.com');
+
+-- 3) UPDATE employee
+--    SET name = 'Jack', birthday = '1950-09-10', email = 'jack@gmail.com'
+--    WHERE id = 1;
+
+-- 4) DELETE FROM employee
+--    WHERE id = 5;
