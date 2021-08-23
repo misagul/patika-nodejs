783e3b3e93a046de35bcd7aed4d88d4168324f3b
M. Isa Gul <71321835+misagul@users.noreply.github.com>
2021-08-23 12:17:04 +0300

    Create hw1.js

---
+++ javascript/hw1.js
@@ -0,0 +1,10 @@
+let name = prompt('Adiniz: ');
+document.querySelector('#myName').innerHTML = name;
+
+
+const today = new Date();
+let h = today.getHours();
+let m = today.getMinutes();
+let s = today.getSeconds();
+
+document.querySelector('#myClock').innerHTML = h+':'+m+':'+s
